d61ef039419e17c693ef8e99adcab61514d00381
AliSheikhAttar <ali.sheikhattar@yahoo.com>
2025-04-26 11:58:00 +0330

    week3

--- Algorithms on graphs/week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ Algorithms on graphs/week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <climits>
 
-using std::vector;
 using std::queue;
+using std::vector;
 
-int bipartite(vector<vector<int> > &adj) {
-  //write your code here
-  return -1;
+int bipartite(vector<vector<int>> &adj)
+{
+  vector<int> distances(adj.size(), INT_MAX);
+  for (size_t j = 0; j < adj.size(); j++)
+  {
+    if (distances[j] == INT_MAX)
+    {
+      std::queue<int> q;
+      q.push(j);
+      distances[j] = 0;
+      while (!q.empty())
+      {
+        auto v = q.front();
+        q.pop();
+        for (int i = 0; i < adj[v].size(); i++)
+        {
+          if (distances[adj[v][i]] == INT_MAX)
+          {
+            q.push(adj[v][i]);
+            distances[adj[v][i]] = distances[v] + 1;
+          }
+          else if (distances[adj[v][i]] == distances[v])
+            return 0;
+        }
+      }
+    }
+  }
+  return 1;
 }
 
-int main() {
+int main()
+{
   int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
+  vector<vector<int>> adj(n, vector<int>());
+  for (int i = 0; i < m; i++)
+  {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
